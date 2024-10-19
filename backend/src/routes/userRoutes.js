d8bbf6dd03aec7fdf5213eb5527c7ffa832417ba
eliasescalante <eliasw.escalante@gmail.com>
2024-10-18 23:59:09 -0300

    delete folder backend

--- backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { registerUser, authUser } from '../controllers/userController.js'
-
-const userRoutes = Router();
-
-userRoutes.post('/register', registerUser);
-userRoutes.get('/login', authUser);
-
-export default userRoutes;
